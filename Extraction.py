ae4fb277514af628eba997ec00c8d6371cc41ae8
QuentinBot <quentin.muench@web.de>
2022-05-21 16:39:52 +0200

    first sentece extraction

--- Extraction.py
+++ Extraction.py
@@ -1,12 +1,16 @@
 from pdfminer.high_level import extract_text
-
+import spacy
 
 def main():
 
+    nlp = spacy.load("en_core_web_sm")
     pdf = extract_text("Doc/articles/malaysia.pdf")
+    doc = nlp(pdf)
 
-    for c in pdf:
-        print(c, end="")
+    for tok in doc:
+        print(tok.text, "-->", tok.dep_, "-->", tok.pos_)
+        if tok.text == ".":
+            break
 
 
 if __name__ == "__main__":
