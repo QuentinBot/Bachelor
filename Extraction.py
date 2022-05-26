2882327a531af0ebecd6f9a1379b0b69a52e7dc7
QuentinBot <quentin.muench@web.de>
2022-05-26 11:02:28 +0200

    working title, doi, author for auckland

--- Extraction.py
+++ Extraction.py
@@ -1,23 +1,42 @@
 from pdfminer.high_level import extract_text
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+import lxml
 import spacy
-from spacy.matcher import Matcher
-from spacy.tokens import Span
 import os
+
+
+def read_tei(tei_file):
+    with open(tei_file, "r") as tei:
+        soup = BeautifulSoup(tei, "lxml-xml")
+        return soup
+    raise RuntimeError("Cannot generate a soup from the input")
 
 
 def main():
 
-    path = "Doc/articles"
+    path = "Doc/articles/XML"
     directories = os.listdir(path)
-    nlp = spacy.load("en_core_web_sm")
+    # nlp = spacy.load("en_core_web_sm")
 
     total_data = []
     i = 0
     for file in directories:
         article_data = {}
-        pdf = extract_text("Doc/articles/" + file)
+        soup = read_tei(path + "/" + file)
+
+        article_data["Title"] = soup.title.getText()
+        article_data["DOI"] = soup.find("idno", type="DOI").getText()
+        first_author = soup.analytic.find("author").persName
+        firstname = first_author.find("forename", type="first").getText()
+        surname = first_author.find("surname").getText()
+        article_data["FirstAuthor"] = surname + ", " + firstname
+
+        """
+        text = extract_text("Doc/articles/" + file)
         print(file)
-        doc = nlp(pdf)
+        doc = nlp(text)
         no_doi = True
 
         for e in doc.ents:
@@ -31,11 +50,12 @@ def main():
             if no_doi and tok.text.__contains__("10."):
                 article_data["doi"] = tok.text
                 no_doi = False
-            # elif no_region and
+        
+        """
 
         total_data.append(article_data)
         i += 1
-        # break
+        break
         if i == 10:
             break
 
